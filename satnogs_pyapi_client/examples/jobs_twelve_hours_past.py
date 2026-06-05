c82fe4b3df7a55171c2cd02a45ab289ba4bb4ed2
Martin Hübner <martin.hubner@web.de>
2026-06-05 14:34:38 +0200

    feat: add twelve-hour python example

---
+++ satnogs_pyapi_client/examples/jobs_twelve_hours_past.py
@@ -0,0 +1,18 @@
+from satnogs_pyapi_client import APIClient, ObservationFilter
+import datetime as dt
+
+c = APIClient("https://network.satnogs.org/api")
+
+now = dt.datetime.utcnow()
+
+# create Jobfilter with keyword-syntax
+f = ObservationFilter(
+    ground_station = 106,
+    start = now - dt.time(hour=12),
+    end = now,
+)
+
+obs = c.get_observations(f)
+
+for ob in obs:
+    print(ob)
